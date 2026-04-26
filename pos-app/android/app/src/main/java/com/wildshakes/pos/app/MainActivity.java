de20ce4b0cf765bb33f7d48ba56c3179a47c1d6c
Clones45 <granadaclyde1@gmail.com>
2026-04-26 06:21:35 -0400

    some changes on the camera

--- pos-app/android/app/src/main/java/com/wildshakes/pos/app/MainActivity.java
+++ pos-app/android/app/src/main/java/com/wildshakes/pos/app/MainActivity.java
@@ -1,5 +1,23 @@
 package com.wildshakes.pos.app;
 
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebViewClient;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Forward WebView camera/mic permission requests to Android's native dialog.
+        // Without this, getUserMedia() silently fails in the Capacitor WebView.
+        bridge.getWebView().setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                runOnUiThread(() -> request.grant(request.getResources()));
+            }
+        });
+    }
+}
