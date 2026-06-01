a3e5688b429a923ff61645e83c3aa2b3bd2880bc
Clones45 <granadaclyde1@gmail.com>
2026-06-01 08:06:27 -0400

    kkk

--- Wildshake-admin/app/app/(franchiser)/franchiser/inventory/page.tsx
+++ Wildshake-admin/app/app/(franchiser)/franchiser/inventory/page.tsx
@@ -1,6 +1,8 @@
 import { createClient } from '@/lib/supabase/server'
 import FranchiserInventoryClient from '@/components/franchiser/FranchiserInventoryClient'
 
+export const dynamic = 'force-dynamic'
+
 export default async function FranchiserInventoryPage() {
   const supabase = await createClient()
   const { data: { user } } = await supabase.auth.getUser()
